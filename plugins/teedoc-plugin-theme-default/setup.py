b83a3277ee79e87b1b69196f7e22aca905c22342
Neucrack <CZD666666@gmail.com>
2021-01-29 22:48:53 +0800

    optimize to top

--- plugins/teedoc-plugin-theme-default/setup.py
+++ plugins/teedoc-plugin-theme-default/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-theme-default',
-    version="1.0.8",
+    version="1.0.9",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
