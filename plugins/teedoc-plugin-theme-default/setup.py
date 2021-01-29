9ebac3afe539e1c03e8d6a5d6a0da66e7de4e3b9
Neucrack <CZD666666@gmail.com>
2021-01-29 22:58:33 +0800

    update footer UI

--- plugins/teedoc-plugin-theme-default/setup.py
+++ plugins/teedoc-plugin-theme-default/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-theme-default',
-    version="1.0.9",
+    version="1.0.10",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
