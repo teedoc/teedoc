edd016fbe961d87fd1e6f470a0c5b8151083d84a
Neucrack <CZD666666@gmail.com>
2021-02-01 17:21:19 +0800

    add assets file

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
+        "teedoc" : ['static/js/*'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
