380d768cd923c5a9167fc3664761ad4e889e2fbb
Neucrack <CZD666666@gmail.com>
2021-02-05 18:00:14 +0800

    fix code UI

--- plugins/teedoc-plugin-theme-default/setup.py
+++ plugins/teedoc-plugin-theme-default/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-theme-default',
-    version="1.1.11",
+    version="1.1.12",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
