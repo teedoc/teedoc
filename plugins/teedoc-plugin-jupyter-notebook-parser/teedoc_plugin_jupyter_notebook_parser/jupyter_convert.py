223b77ff5f7bdb4774bf898179994808402ca7dc
Neucrack <CZD666666@gmail.com>
2021-03-09 00:27:21 +0800

    update jupyter notebook support

--- plugins/teedoc-plugin-jupyter-notebook-parser/teedoc_plugin_jupyter_notebook_parser/jupyter_convert.py
+++ plugins/teedoc-plugin-jupyter-notebook-parser/teedoc_plugin_jupyter_notebook_parser/jupyter_convert.py
@@ -1,11 +1,18 @@
 
+
+from nbconvert.exporters.templateexporter import TemplateExporter
 from nbconvert import HTMLExporter
 import nbformat
 import re
+import os
 
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(curr_dir, "templates")
+
+TemplateExporter.extra_template_basedirs=[templates_dir]
 html_exporter = HTMLExporter()
-html_exporter.template_file = "basic"
-
+html_exporter.template_name = "lab"
+html_exporter.template_file = 'base.html.j2'
 
 def convert_ipynb_to_html_body(path):
     global html_exporter
@@ -64,26 +71,37 @@ def convert_ipynb_to_html(path):
         html.desc = html.metadata["desc"]
         html.tags = html.metadata["tags"].split(",") if html.metadata["tags"] else []
         body, resources = html_exporter.from_notebook_node(content)
+        # body = body.replace("<pre>", '<pre class="language-python">')
+        # print(body)
         html.body = body
     return html
 
 if __name__ == "__main__":
     import sys
-    notebook = sys.argv[1]
+    notebook = "e:/main/projects/teedoc/examples/local_test/docs/get_started/zh/syntax/syntax_notebook.ipynb"
+    # notebook = sys.argv[1]
+    TemplateExporter.extra_template_basedirs=[templates_dir]
     html_exporter = HTMLExporter()
     # print(html_exporter.template_name)
-    # html_exporter.template_name = 'basic'
-    html_exporter.template_file = "basic"
+    html_exporter.template_name = 'lab'
+    html_exporter.template_file = "base.html.j2"
+    print(TemplateExporter.extra_template_basedirs)
 
-    with open(notebook) as f:
+    with open(notebook, encoding="utf-8") as f:
         content = nbformat.read(f, as_version=4)
         body, resources = html_exporter.from_notebook_node(content)
-        # print(body)
+        # from html_toc import HtmlTocParser
+        # parser = HtmlTocParser()
+        # parser.feed(body)
+        # print(parser.toc())
+        # html = parser.toc_html(depth=2, lowest_level=5)
+        # print(html)
+        print(body)
         # print(type(resources))
-        print("Resources:", resources.keys())
-        print("Metadata:", resources['metadata'].keys())
-        print("Inlining:", resources['inlining'].keys())
-        print("Extension:", resources['output_extension'])
-        print(len(resources['inlining']["css"]))
-        with open("out/a.css", "w") as f:
-            f.write(resources['inlining']["css"][1])
+        # print("Resources:", resources.keys())
+        # print("Metadata:", resources['metadata'].keys())
+        # print("Inlining:", resources['inlining'].keys())
+        # print("Extension:", resources['output_extension'])
+        # print(len(resources['inlining']["css"]))
+        # with open("out/a.css", "w") as f:
+        #     f.write(resources['inlining']["css"][1])
