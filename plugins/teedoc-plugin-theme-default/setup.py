dc25fbd29fcf7e2258200951b12a493c4d5a34ba
Neucrack <CZD666666@gmail.com>
2021-08-09 21:24:08 +0800

    update version

--- plugins/teedoc-plugin-theme-default/setup.py
+++ plugins/teedoc-plugin-theme-default/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-theme-default',
-    version="1.9.3",
+    version="1.9.4",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
