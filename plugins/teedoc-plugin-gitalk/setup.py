1292d9050cd54b7952c315f81acdb6813138a3a3
Neucrack <CZD666666@gmail.com>
2021-09-02 21:48:54 +0800

    fix gittalk url find bug

--- plugins/teedoc-plugin-gitalk/setup.py
+++ plugins/teedoc-plugin-gitalk/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-gitalk',
-    version="1.1.2",
+    version="1.1.3",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
