3410dd1eadb55fd2928ad0cf4e6e00e0b816acfa
Neucrack <CZD666666@gmail.com>
2021-09-03 18:22:24 +0800

    update gitalk version

--- plugins/teedoc-plugin-gitalk/setup.py
+++ plugins/teedoc-plugin-gitalk/setup.py
@@ -13,7 +13,7 @@ print("packages:", packages)
 
 setup(
     name='teedoc-plugin-gitalk',
-    version="1.2.4",
+    version="1.2.5",
     author='Neucrack',
     author_email='CZD666666@gmail.com',
 
