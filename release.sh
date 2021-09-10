e2697bc8fccb8d1c5b335c84639185882ad73a81
Neucrack <CZD666666@gmail.com>
2021-09-11 02:11:41 +0800

    update template

--- release.sh
+++ release.sh
@@ -5,6 +5,10 @@ set -e
 if [[ "${1}x" != "x" ]]; then
     cd $1
 fi
+if [[ !-f teedoc/template/site_config.json ]]; then
+    echo "submodule need pull"
+    exit 1
+fi
 rm -rf dist build
 python setup.py sdist bdist_wheel
 twine upload dist/*
