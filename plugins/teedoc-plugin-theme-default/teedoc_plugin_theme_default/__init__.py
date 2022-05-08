e52e111cfc233b8268c0ce17ff04b0f47f839563
Neucrack <CZD666666@gmail.com>
2022-05-08 22:53:21 +0800

    optimize toc icon z-index

--- plugins/teedoc-plugin-theme-default/teedoc_plugin_theme_default/__init__.py
+++ plugins/teedoc-plugin-theme-default/teedoc_plugin_theme_default/__init__.py
@@ -12,7 +12,7 @@ except Exception:
 from teedoc import Plugin_Base
 from teedoc import Fake_Logger
 
-__version__ = "1.19.3"
+__version__ = "1.19.4"
 
 class Plugin(Plugin_Base):
     name = "teedoc-plugin-theme-default"
