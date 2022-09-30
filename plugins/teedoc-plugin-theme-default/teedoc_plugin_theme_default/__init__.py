c69a8e601819caaba8550aef9acfdb467c994279
Neucrack <CZD666666@gmail.com>
2022-09-30 14:17:16 +0800

    fix sidebar scrollbar bug

--- plugins/teedoc-plugin-theme-default/teedoc_plugin_theme_default/__init__.py
+++ plugins/teedoc-plugin-theme-default/teedoc_plugin_theme_default/__init__.py
@@ -12,7 +12,7 @@ except Exception:
 from teedoc import Plugin_Base
 from teedoc import Fake_Logger
 
-__version__ = "1.21.4"
+__version__ = "1.21.5"
 
 class Plugin(Plugin_Base):
     name = "teedoc-plugin-theme-default"
